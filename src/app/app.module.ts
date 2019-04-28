30ab78323333675f4ebc5aaa53f9ec91aa19740a
Robert Luo <robertxluo@gmail.com>
2019-04-28 00:58:43 -0500

    Add body container component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ButtonLayoutComponent } from './button-layout/button-layout.component';
+import { BodyContainerComponent } from './button-layout/body-container/body-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonLayoutComponent
+    ButtonLayoutComponent,
+    BodyContainerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/button-layout/body-container/body-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body-container',
+  templateUrl: './body-container.component.html',
+  styleUrls: ['./body-container.component.css']
+})
+export class BodyContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
