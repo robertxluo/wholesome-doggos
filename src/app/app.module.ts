d3b0d62348f4006c577126e80eba59002ab50531
Robert Luo <robertxluo@gmail.com>
2019-07-17 20:23:52 -0500

    Refactor files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ButtonLayoutComponent } from './button-layout/button-layout.component';
-import { BodyContainerComponent } from './button-layout/body-container/body-container.component';
+import { ButtonLayoutComponent } from './button-layout-section/button-layout.component';
+import { BodyContainerComponent } from './body-container/body-container.component';
 
 @NgModule({
   declarations: [
