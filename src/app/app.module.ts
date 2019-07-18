fd325673b14d27d7d089a5703bcabe51780c63d2
Robert Luo <robertxluo@gmail.com>
2019-07-17 21:35:28 -0500

    Add footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ButtonLayoutComponent } from './button-layout-section/button-layout.component';
 import { BodyContainerComponent } from './body-container/body-container.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ButtonLayoutComponent,
-    BodyContainerComponent
+    BodyContainerComponent,
+    FooterComponent
   ],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
